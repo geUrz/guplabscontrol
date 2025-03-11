09ba6fd624c5a557cae64ce5ce9455b75aacaafa
geUrz <gerardourzua20@gmail.com>
2025-03-11 09:21:28 -0700

    start 2

--- src/libs/db.js
+++ src/libs/db.js
@@ -4,7 +4,7 @@ import mysql from 'mysql2/promise';
 const connection  = mysql.createPool({
   host: 'localhost',   
   user: 'root',           
-  password: 'root',  
+  password: '',  
   database: 'clicknetmxcontrol', 
   waitForConnections: true,
   connectionLimit: 10,
