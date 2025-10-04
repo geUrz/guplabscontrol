8f2a793c469ce54e551f46a050bb5b8fa780a128
geUrz <gerardourzua20@gmail.com>
2025-10-04 16:01:39 -0700

    server1

--- src/libs/db.js
+++ src/libs/db.js
@@ -4,8 +4,8 @@ import mysql from 'mysql2/promise';
 const connection  = mysql.createPool({
   host: 'localhost',   
   user: 'root',           
-  password: 'root',  
-  database: 'clicknetcontrol', 
+  password: 'adm1nsql1',  
+  database: 'guplabscontrol', 
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
